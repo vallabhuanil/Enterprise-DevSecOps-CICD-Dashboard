0d49d9eff226bbda12fb2400a5262e80c53f541b
vallabhuanil <vallabhuanilsai2@gmail.com>
2026-05-27 15:46:25 +0530

    Create MailtrapEmailService.java

---
+++ backend/src/main/java/com/devsecops/dashboard/service/MailtrapEmailService.java
@@ -0,0 +1,46 @@
+@Service
+public class MailtrapEmailService {
+
+    @Value("${MAILTRAP_TOKEN}")
+    private String token;
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public void sendOtp(String toEmail, String otp) {
+
+        String url = "https://send.api.mailtrap.io/api/send";
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(token);
+
+        String body = """
+        {
+          "from": {
+            "email": "hello@demomailtrap.co",
+            "name": "DevSecOps Dashboard"
+          },
+          "to": [
+            {
+              "email": "%s"
+            }
+          ],
+          "subject": "Your OTP Code",
+          "text": "Your OTP is: %s"
+        }
+        """.formatted(toEmail, otp);
+
+        HttpEntity<String> request =
+                new HttpEntity<>(body, headers);
+
+        ResponseEntity<String> response =
+                restTemplate.postForEntity(
+                        url,
+                        request,
+                        String.class
+                );
+
+        System.out.println(response.getBody());
+    }
+}
